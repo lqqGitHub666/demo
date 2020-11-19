12aca19ead4d4f36f3578c7142f51c315041eb57
liuqingqing <liuqingqing@biosan.cn>
2020-11-19 11:23:25 +0800

    希尔排序算法

--- src/main/java/com/example/demo/data_structures/sortstructures/InsertionSort.java
+++ src/main/java/com/example/demo/data_structures/sortstructures/InsertionSort.java
@@ -11,7 +11,8 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{101,34,119,10,18,9,1};
-        insertSort(arr);
+//        insertSort(arr);
+        insertSort2(arr);
     }
 
     //插入排序，时间复杂度O(n²)
@@ -24,6 +25,20 @@ public class InsertionSort {
                 insertIndex --;
             }
             arr[insertIndex+1] = insertVal;
+            System.out.println("第( "+i+" )轮排序后的数组：");
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    public static void insertSort2(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            for ( int j = i-1; j >=0 ; j--) {
+                int temp = arr[j];
+                if (arr[j] > arr[j+1]){
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
             System.out.println("第( "+i+" )轮排序后的数组：");
             System.out.println(Arrays.toString(arr));
         }
