24d0e512fbcf1d4aba57fe7f15702499e9777e8e
liuqingqing <liuqingqing@biosan.cn>
2020-11-25 09:34:46 +0800

    search

--- src/main/java/com/example/demo/data_structures/searchstructures/BinarySearch.java
+++ src/main/java/com/example/demo/data_structures/searchstructures/BinarySearch.java
@@ -1,5 +1,8 @@
 package com.example.demo.data_structures.searchstructures;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @ClassName: DichotomySearch
  * @Description: TODO
@@ -10,9 +13,9 @@ package com.example.demo.data_structures.searchstructures;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,4,5,6,12,33,45,789,1235,3366};
-        int i = binarySearch(arr, 0, arr.length, 121);
-        System.out.println(i);
+        int[] arr = new int[]{1,2,3,4,5,6,12,12,12,12,12,12,33,45,789,1235,3366};
+        List list = binarySearchRepeat(arr, 0, arr.length - 1, 12);
+        System.out.println(list);
     }
 
     public static int binarySearch(int[] arr,int left,int right, int val){
@@ -23,9 +26,42 @@ public class BinarySearch {
         if (val == arr[midIndex]){
             return midIndex;
         }else if (val > arr[midIndex]){
-            return binarySearch(arr,midIndex,right,val);
+            return binarySearch(arr,midIndex+1,right,val);
         }else {
-            return binarySearch(arr,left,midIndex,val);
+            return binarySearch(arr,left,midIndex-1,val);
+        }
+    }
+
+    //找到所有的满足条件的数据都找出来
+    public static List binarySearchRepeat(int[] arr, int left, int right, int val){
+        if (right < left){
+            return  null;
+        }
+        int midIndex = (left+right)/2;
+        if (val == arr[midIndex]){
+            List<Integer> list = new ArrayList<>();
+            int index = midIndex -1;
+            while (true){
+                if (index < 0 || arr[index] != val){
+                    break;
+                }
+                list.add(index);
+                index --;
+            }
+            list.add(midIndex);
+            index = midIndex + 1;
+            while (true){
+                if (index > arr.length || arr[index] != val){
+                    break;
+                }
+                list.add(index);
+                index ++;
+            }
+            return list;
+        }else if (val > arr[midIndex]){
+            return binarySearchRepeat(arr,midIndex+1,right,val);
+        }else {
+            return binarySearchRepeat(arr,left,midIndex-1,val);
         }
     }
 }
--- src/main/java/com/example/demo/data_structures/searchstructures/SeqSearch.java
+++ src/main/java/com/example/demo/data_structures/searchstructures/SeqSearch.java
@@ -8,4 +8,25 @@ package com.example.demo.data_structures.searchstructures;
  * @Version: 1.0
  */
 public class SeqSearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,9,11,-1,34,89};
+        int i = seqSearch(arr, -1);
+        if (i == -1){
+            System.out.println("没找到");
+        }else {
+            System.out.println("下表为  "+ i);
+        }
+
+    }
+
+    public static int seqSearch(int[] arr,int value){
+        for (int i = 0; i < arr.length; i++) {
+            if (value == arr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
