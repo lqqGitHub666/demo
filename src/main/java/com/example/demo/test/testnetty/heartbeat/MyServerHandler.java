c13c13dd1b82cf9da8a8f42b23b71f998cf3b647
liuqingqing <liuqingqing@biosan.cn>
2021-01-26 00:04:34 +0800

    netty

---
+++ src/main/java/com/example/demo/test/testnetty/heartbeat/MyServerHandler.java
@@ -0,0 +1,36 @@
+package com.example.demo.test.testnetty.heartbeat;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleStateEvent;
+
+/**
+ * @author 作者 lqq
+ * @ClassName 类名 MyServerHandler
+ * @date 2021/1/25 23:29
+ * @注释：
+ */
+public class MyServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+
+        if (evt instanceof IdleStateEvent){
+            IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
+            String eventType = "";
+            switch (idleStateEvent.state()){
+                case READER_IDLE:
+                    eventType = "读空闲";
+                    break;
+                case WRITER_IDLE:
+                    eventType = "写空闲";
+                    break;
+                case ALL_IDLE:
+                    eventType = "读写空闲";
+                    break;
+            }
+            System.out.println(ctx.channel().remoteAddress() + "--超时时间--" + eventType);
+            System.out.println("服务器做相应处理..");
+        }
+    }
+}
